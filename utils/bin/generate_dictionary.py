52f8d6d4eb312407e3f3a5000d35e94d179507b0
bibreen <bibreen@gmail.com>
2014-01-19 23:19:14 +0900

    * 수정: seed로 사전 파일 생성하도록 코드 수정

--- utils/bin/generate_dictionary.py
+++ utils/bin/generate_dictionary.py
@@ -4,11 +4,6 @@ import csv
 import getpass
 import os
 import sys
-<<<<<<< HEAD
-=======
-base_dir = os.path.dirname(__file__) + '/../'
-sys.path.append(base_dir)
->>>>>>> 374aec5679a20608fcd9349905fbd732cb95bcbe
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -19,12 +14,8 @@ from dictionary.utils import get_end_with_jongsung
 
 engine = None
 db_session = None
-<<<<<<< HEAD
 base_dir = os.path.dirname(os.path.realpath(__file__))
 target_dir = base_dir + '/../../seed'
-=======
-target_dir = base_dir + '/../data'
->>>>>>> 374aec5679a20608fcd9349905fbd732cb95bcbe
 
 
 def connect_db(db_type, host, database, user, password):
