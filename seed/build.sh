3b04046e0f40a5db5357827ca11c7bf97d801886
Yong-woon Lee <bibreen@gmail.com>
2014-04-19 17:29:24 +0900

    * 수정: build.sh에 스크립트 작동하도록 주석 제거 * 수정: test 정답 셋 변경

--- seed/build.sh
+++ seed/build.sh
@@ -27,9 +27,9 @@ $DICT_GEN -o ../final -m $model_file
 rm -rf ../final.org
 cp -R ../final ../final.org
 
-#./change_word_cost_ex.sh
-#./change_word_cost.sh
-#./change_connection_cost.sh
+./change_word_cost_ex.sh
+./change_word_cost.sh
+./change_connection_cost.sh
 
 pushd ../final
 ./configure; make
